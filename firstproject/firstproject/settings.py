5933f73d25ea80628f9c9e3a19837859e83f26ce
Николай Боровков <nikolai.borovkov@gmail.com>
2022-04-16 00:14:40 +0300

    add main

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)hg+lg!t(9iajxo2$kx564ut5abmd8y!l)$lqaz9c-%-znl*o@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
