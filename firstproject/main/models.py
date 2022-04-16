a77edc3d74712aad3d374aec23bb28eda46ed6f4
Николай Боровков <nikolai.borovkov@gmail.com>
2022-04-16 11:59:11 +0300

    add templates

--- firstproject/main/models.py
+++ firstproject/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Recepts(models.Model):
+    title = models.CharField('Название', max_length=50)
+    ingridients = models.CharField('Ингридиенты', max_length=250)
+    recept_text = models.TextField('Рецепт')
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
